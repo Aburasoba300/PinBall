42657bb2489949be6f8af917cff3ee13ffa15ca5
Aburasoba300 <hiroto2001_0122@yahoo.co.jp>
2024-11-16 02:08:12 +0900

    得点の表記

--- Assets/ScoreController.cs
+++ Assets/ScoreController.cs
@@ -6,54 +6,50 @@ using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour
 {
-    //Materialを入れる
-    Material myMaterial;
-    //角度
-    private int degree = 0;
-
     //得点を表示するテキスト
-    private GameObject scoreText;
+    private Text scoreText;
     //得点
     private int score = 0;
 
-    //オブジェクトの点数
-    private int a;
+    int a;
 
-    // Start is called before the first frame update
-    void Start()
+
+    //衝突時に呼ばれる関数
+    private void OnCollisionEnter(Collision other)
     {
-        //シーン中のScoreオブジェクトを取得
-        this.scoreText = GameObject.Find("Score");
         //タグによって得点を変える
-        if (tag == "SmallStar") 
+        if (tag == "SmallStarTag")
         {
             a = 5;
         }
-        else if (tag == "LargeStar") 
+        else if (tag == "LargeStarTag")
         {
             a = 10;
         }
-        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag") 
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
         {
             a = 20;
         }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //スコアを加算
+        score += a;
+
+        //シーン中のScoreオブジェクトを取得
+        this.scoreText = GameObject.Find("Score").GetComponent<Text>();
+
+        //int型をstring型に変換
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.degree >= 0)
-        {
-            //衝突時scoreに得点を加算する
-            score += a;
-
-        }
+        
     }
-    //衝突時に呼ばれる関数
-    private void OnCollisionEnter(Collision other)
-    {
-        //角度を180に設定
-        this.degree = 180;
-    }
-    scoreText.GetComponent<Text>().text = score;
+ 
 }
